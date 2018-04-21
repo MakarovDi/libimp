852c0807146790c535829fe4f7607a60b3d421c2
Dmitry Makarov <MakarovDmV@gmail.com>
2018-04-21 20:57:03 +0300

    split matrix equality operator for different types

--- test/common/matrix_operator.cpp
+++ test/common/matrix_operator.cpp
@@ -22,24 +22,34 @@ TYPED_TEST_CASE(MatrixOperatorTypeMixture, MathTypes);
 
 TYPED_TEST(MatrixOperatorTypeMixture, equality_operator)
 {
-    ASSERT_TRUE( Matrix<TypeParam>({ TypeParam(1), TypeParam(2) }) == Matrix<TypeParam>({ TypeParam(1), TypeParam(2) }) );
-    ASSERT_TRUE( Matrix<TypeParam>({ TypeParam(1) }) != Matrix<TypeParam>({ TypeParam(1), TypeParam(2) }) );
+    using T = TypeParam;
 
-    Matrix<TypeParam> m1(2, 1, { TypeParam(1), TypeParam(2) });
-    Matrix<TypeParam> m2(1, 2, { TypeParam(1), TypeParam(2) });
+    T eps = std::numeric_limits<T>::epsilon();
+
+    ASSERT_TRUE( Matrix<T>({ T(T(1) + eps), T(2) }) == Matrix<T>({ T(1), T(2) }) );
+
+    if (std::is_floating_point<T>::value)
+    {
+        ASSERT_TRUE( Matrix<T>({ T(T(1) + 2*eps), T(2) }) != Matrix<T>({ T(1), T(2) }) );
+    }
+
+    ASSERT_TRUE( Matrix<T>({ T(1) }) != Matrix<T>({ T(1), T(2) }) );
+
+    Matrix<T> m1(2, 1, { T(1), T(2) });
+    Matrix<T> m2(1, 2, { T(1), T(2) });
 
     ASSERT_TRUE(m1 != m2);
     ASSERT_TRUE(m1 == m1);
 
-    Matrix<TypeParam> m3({
-         { TypeParam(1), TypeParam(2) },
-         { TypeParam(3), TypeParam(4) },
-         { TypeParam(5), TypeParam(6) },
+    Matrix<T> m3({
+         { T(1), T(2) },
+         { T(3), T(4) },
+         { T(5), T(6) },
     });
 
-    Matrix<TypeParam> m4(2, 3, { TypeParam(1), TypeParam(2),
-                                 TypeParam(3), TypeParam(4),
-                                 TypeParam(5), TypeParam(6) });
+    Matrix<T> m4(2, 3, { T(1), T(2),
+                         T(3), T(4),
+                         T(5), T(6) });
 
     ASSERT_TRUE(m3 == m4);
 }
